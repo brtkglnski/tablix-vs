b6b05dc78ce98eba957b0bcd0f470f659599829c
brtkglnski <glinskib2008@gmail.com>
2025-01-10 20:49:33 +0100

    juice do tekstu

--- Skrypty/JS/comparison.js
+++ Skrypty/JS/comparison.js
@@ -2,6 +2,8 @@ document.addEventListener("mousemove", (event) => {
     const mainGameLayout = document.querySelector(".mainGameLayout");
     const leftOption = document.querySelector(".leftOption");
     const rightOption = document.querySelector(".rightOption");
+    const leftText = leftOption.querySelector("p");
+    const rightText = rightOption.querySelector("p");
   
     const mouseX = event.clientX / window.innerWidth;
   
@@ -11,7 +13,16 @@ document.addEventListener("mousemove", (event) => {
   
     leftOption.style.flex = leftWidth;
     rightOption.style.flex = rightWidth;
-  });
+    
 
+    const baseSize = 2;
+    const maxIncrease = 2.5;
+    
+    const leftTextSize = baseSize + maxIncrease * (1 - mouseX);
+    const rightTextSize = baseSize + maxIncrease * mouseX;
+    
+    leftText.style.fontSize = `${leftTextSize}rem`;
+    rightText.style.fontSize = `${rightTextSize}rem`;
+  });
 
 
